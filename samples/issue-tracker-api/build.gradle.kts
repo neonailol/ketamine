1d557ac69f2ea9feffc600a14af37acedebb071c
Valeriy Zhirnov <neonailol@gmail.com>
2018-02-18 22:22:46 +0300

    Stub for sample

---
+++ samples/issue-tracker-api/build.gradle.kts
@@ -0,0 +1,21 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    application
+    kotlin("jvm")
+}
+
+dependencies {
+    compile(project(":modules:ketamine-models"))
+    compile(kotlin("stdlib-jdk8"))
+    compile(kotlin("reflect"))
+    compile("com.fasterxml.jackson.core:jackson-core:2.9.4")
+    compile("com.fasterxml.jackson.core:jackson-annotations:2.9.4")
+    compile("com.fasterxml.jackson.core:jackson-databind:2.9.4")
+    compile("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.4")
+    compile("com.fasterxml.jackson.module:jackson-module-parameter-names:2.9.4")
+    compile("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.9.4")
+    compile("org.slf4j:slf4j-api:1.7.25")
+    compile("ch.qos.logback:logback-core:1.2.3")
+    compile("ch.qos.logback:logback-classic:1.2.3")
+}
---
+++ samples/issue-tracker-api/src/main/kotlin/nnl/rocks/ketamine/samples/Main.kt
@@ -0,0 +1,5 @@
+package nnl.rocks.ketamine.samples
+
+fun main(args: Array<String>) {
+
+}
