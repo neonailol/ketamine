706b3119e894446cdb9f477c29608a72fd61882a
Valeriy Zhirnov <neonailol@gmail.com>
2018-02-23 16:59:31 +0300

    Define operation

---
+++ modules/ketamine-models/src/main/kotlin/nnl/rocks/ketamine/models/domain/Net.kt
@@ -0,0 +1,13 @@
+package nnl.rocks.ketamine.models.domain
+
+enum class HttpMethod {
+    GET,
+    HEAD,
+    POST,
+    PUT,
+    DELETE,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATH
+}
--- modules/ketamine-models/src/main/kotlin/nnl/rocks/ketamine/models/operations/Operation.kt
+++ modules/ketamine-models/src/main/kotlin/nnl/rocks/ketamine/models/operations/Operation.kt
@@ -1,6 +1,15 @@
 package nnl.rocks.ketamine.models.operations
 
+import nnl.rocks.ketamine.models.domain.Description
+import nnl.rocks.ketamine.models.domain.HttpMethod
+import nnl.rocks.ketamine.models.domain.Summary
+
 /**
  * Represents an atomic rest request
  */
-class Operation
+class Operation(
+    private val summary: Summary,
+    private val description: Description,
+    private val method: HttpMethod,
+    private val path: String
+)
